55315a8f757913ec8961b6de53a6418995b6065c
Sole <30993566+sonosole@users.noreply.github.com>
2025-04-19 13:53:05 +0800

    update: limit the range of parameter b in [0,1]

--- src/6-stretch.jl
+++ src/6-stretch.jl
@@ -1,14 +1,15 @@
 export stretch
 
 
+# 将 [a,b] 比例区间的值线性映射到 [0,1] 比例区间 
 """
     stretch(s::AbstractArray{T},
             a::AbstractFloat=rand(0.02:0.02:0.1),
             b::AbstractFloat=rand(0.9:0.02:0.98); dim::Int=1) where T <: Real
 
 Stretch `s` along dimension `dim`.
-0 ≤ `a`, is the starting point, 
-0 ≤ `b`, is the ending point.
+0 ≤ `a`≤ 1, is the starting point, 
+0 ≤ `b`≤ 1, is the ending point.
 If `a` < `b`, then stretch the proportional region [a,b] into [0,1] along dimension `dim`. 
 If `a` > `b`, then stretch the proportional region [b,a] into [0,1] along dimension `dim` after flipping `s` along dimension `dim`. 
 # Math
@@ -22,6 +23,7 @@ function stretch(s::AbstractArray{T},
                  a::AbstractFloat=rand(0.02:0.02:0.1),
                  b::AbstractFloat=rand(0.9:0.02:0.98); dim::Int=1) where T <: Real
     @assert 0 ≤ a ≤ 1 "0 ≤ a ≤ 1, but got $a"
+    @assert 0 ≤ b ≤ 1 "0 ≤ b ≤ 1, but got $b"
     k = T(b - a)
     a = T(a)
 
